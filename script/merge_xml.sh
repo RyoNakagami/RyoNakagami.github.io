df5bf2dff3166abef822b3074cc513d77cf99089
RyoNak <nakagamiryo0901@gmail.com>
2025-08-07 09:32:34 +0900

    TEST: xml remove header spaces

--- script/merge_xml.sh
+++ script/merge_xml.sh
@@ -21,9 +21,9 @@ sed -i '$d' $TARGET_FILE
 printf '%s\n' "$external_urls" >> $TARGET_FILE
 echo '</urlset>' >> $TARGET_FILE
 
+# Ensure well-formed XML using xmllint (optional, safe)
+sed 's/^[[:space:]]*//' "$TARGET_FILE" > "${TARGET_FILE}.tmp" && mv "${TARGET_FILE}.tmp" "$TARGET_FILE"
+
 # logging the merge
 echo "📄 Last 20 lines of sitemap.xml:"
 tail -n 20 $TARGET_FILE
-
-# Ensure well-formed XML using xmllint (optional, safe)
-sed 's/^[[:space:]]*//' "$TARGET_FILE" > "${TARGET_FILE}.tmp" && mv "${TARGET_FILE}.tmp" "$TARGET_FILE"
